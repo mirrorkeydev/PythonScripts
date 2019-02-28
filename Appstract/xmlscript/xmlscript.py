912eb5ac97682890b0335ef53f586786e8e4fb45
Melanie <mirrorkeydev@gmail.com>
2019-02-28 13:58:57 -0800

    changed formatting for CandyBar Dashboard

--- Appstract/xmlscript/xmlscript.py
+++ Appstract/xmlscript/xmlscript.py
@@ -3,9 +3,9 @@ Given: a local directory filled with png icons in the format airbnb.png, etc
 
 Return: a text file with all icons' filenames in the xml format:
 
-        <item>airbnb</item>
-        <item>bing</item>
-        <item>chrome</item>
+        <item drawable="airbnb" />
+        <item drawable ="bing" />
+        <item drawable = "chrome" />
         ...
 """
 
@@ -13,7 +13,7 @@ import os,sys
 
 os.chdir("C:\\Users\\Melanie\\Documents\\GitHub\\PythonScripts\\Appstract\\xmlscript")
 
-path = "C:\\Users\\Melanie\\Documents\\GitHub\\Appstract\\app\\src\\main\\res\\drawable-nodpi"
+path = "C:\\Users\\Melanie\\Documents\\Appstract\\candybar-sample-master\\app\\src\\main\\res\\drawable-nodpi"
 
 dirs = os.listdir(path)
 
@@ -22,6 +22,6 @@ f = open("xmloutput.txt","w")
 for element in dirs:
     print(element)
     element = element[0:(len(element))-4]
-    f.write("\n<item>" + str(element) + "</item>")
+    f.write("\n<item drawable =\"" + str(element) + "\" />")
     
 f.close()
